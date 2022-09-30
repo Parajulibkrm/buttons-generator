24ac16a8f4ed55b6384c24dc622ab1ae7b9f998b
laindomJS <adrijar21@gmail.com>
2022-09-30 12:54:47 -0400

    added copy funcionality and toast

--- src/components/button/Button.tsx
+++ src/components/button/Button.tsx
@@ -1,7 +1,29 @@
-import { ButtonProps } from "../../types/ButtonTypes";
+import { ButtonProps } from '../../types/ButtonTypes';
+import { CopyToClipboard } from 'react-copy-to-clipboard';
+import toast, { Toaster } from 'react-hot-toast';
 
-export const Button = ({ styleButton }:ButtonProps): JSX.Element => {
+export const Button = ({ styleButton }: ButtonProps): JSX.Element => {
+	
+	const styleToCopy: string = 'Hello World!';
+
 	return (
-		<button className={styleButton}>Hello World</button>
+		<>
+			<CopyToClipboard text={styleToCopy}>
+				<button
+					className={styleButton}
+					onClick={() => toast('Texto copiado', { position: 'bottom-center' })}
+				>
+					Hello World
+				</button>
+			</CopyToClipboard>
+			<Toaster
+				toastOptions={{
+					style: {
+						background: 'green',
+						color: 'white',
+						fontWeight: 'bold',
+					},
+				}} />
+		</>
 	)
 }
